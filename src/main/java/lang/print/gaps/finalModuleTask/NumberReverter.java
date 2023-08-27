a3c40fda42ed8b2d5d5a4bcd2dbc0e05b463556f
shenanigami <38479116+shenanigami@users.noreply.github.com>
2023-08-27 13:45:35 +0600

    First batch of changes

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int magnitude = 1;
+        int newNum = 0;
+        while (number > 0) {
+           newNum *= 10;
+           newNum += number % 10;
+           number /= 10;
+        }
+        System.out.println(newNum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        float tempF = (float) ((temperatureCelsius * 9/5.0) + 32);
+        System.out.println(tempF);
     }
 }
